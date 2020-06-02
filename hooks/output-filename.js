6a647fbdc156a17a549059a6a4f3705db53307e0
Julian Rabe <mail@julianrabe.com>
2020-06-02 12:03:58 +0200

    Changed rename to renameSync

--- hooks/output-filename.js
+++ hooks/output-filename.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 module.exports = {
     'generate:after': generator => {
         if(generator.templateParams.outFilename !== 'asyncapi.md') {
-            fs.rename(`${generator.targetDir}/asyncapi.md`,
-                `${generator.targetDir}/${generator.templateParams.outFilename}`, () => {});
+            fs.renameSync(`${generator.targetDir}/asyncapi.md`,
+                `${generator.targetDir}/${generator.templateParams.outFilename}`);
         }
     }
 }
