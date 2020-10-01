e7a345a97d54393d228b0929492ed8c04dc3a629
mbeuil <33709157+mbeuil@users.noreply.github.com>
2020-10-01 17:20:50 +0200

    refactor: Remove unnecessary semicolon from filters (#38)

--- filters/all.js
+++ filters/all.js
@@ -6,23 +6,23 @@ function tree(path) {
     const dottedPath = filteredPaths.join('.');
 
     return `${dottedPath}.`;
-};
+}
 filter.tree = tree;
 
 function buildPath(propName, path) {
     if (!path) return propName;
     return `${path}.${propName}`;
-};
+}
 filter.buildPath = buildPath;
 
 function isRequired(obj, key) {
     return obj && Array.isArray(obj.required) && !!(obj.required.includes(key));
-};
+}
 filter.isRequired = isRequired;
 
 function acceptedValues(items) {
     if (!items) return '<em>Any</em>';
 
     return items.map(i => `<code>${i}</code>`).join(', ');
-};
+}
 filter.acceptedValues = acceptedValues;
