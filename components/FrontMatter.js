6d8eb9992f97717085b5880cbc847eedfe044bfe
Mike Ralphson <mike.ralphson@gmail.com>
2021-02-24 13:13:55 +0000

    refactor in components/FrontMatter.js
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- components/FrontMatter.js
+++ components/FrontMatter.js
@@ -4,24 +4,21 @@ import * as fs from "fs";
 import * as yaml from "yaml";
 
 export function FrontMatter({ asyncapi, params }) {
-
   if (!(params.slate || params.frontMatter)) return '';
-
-  const defaultFM = {
-    title: asyncapi.info().title(),
-    language_tabs: [],
-    toc_footers: [],
-    includes: [],
-    search: true,
-    code_clipboard: true
-  };
 
   let frontMatter;
   if (params.frontMatter) {
     frontMatter = yaml.parse(fs.readFileSync(params.frontMatter,'utf8'));
+  } else {
+    frontMatter = {
+      title: asyncapi.info().title(),
+      language_tabs: [],
+      toc_footers: [],
+      includes: [],
+      search: true,
+      code_clipboard: true
+    };
   }
-
-  const frontMatterStr = yaml.stringify(frontMatter || defaultFM);
+  const frontMatterStr = yaml.stringify(frontMatter);
   return <Text newLines={2}>{`---\n${frontMatterStr}\n---`}</Text>
 }
-
