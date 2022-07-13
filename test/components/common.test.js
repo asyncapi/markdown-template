40fa50d7e7b84bdd8e50d0a3dd30c45dc0c5b8d5
Michael Gerischer <michael.gerischer@peerigon.com>
2022-07-13 14:57:57 +0200

    test: added tests for Table and helper func

--- test/components/common.test.js
+++ test/components/common.test.js
@@ -1,6 +1,32 @@
 import { render } from '@asyncapi/generator-react-sdk';
 
-import { TableRow } from '../../components/common';
+import { Table, TableRow } from '../../components/common';
+
+describe('Table', () => {
+  it('should escape pipe operators in cell values', () => {
+    const headers = ['Name', 'Type', 'Description', 'Value', 'Constraints', 'Notes'];
+
+    const data = [[
+      '(root)',
+      'string',
+      '-',
+      'allowed (`"foo|bar|baz"`)',
+      'pattern (`/foo\\|bar/`)',
+      '-',
+    ]];
+    const rowRenderer = (it) => it;
+
+    const expected = `
+| Name | Type | Description | Value | Constraints | Notes |
+|---|---|---|---|---|---|
+| (root) | string | - | allowed (\`"foo\\|bar\\|baz"\`) | pattern (\`/foo\\\\|bar/\`) | - |
+    `;
+
+    const result = render(<Table headers={headers} rowRenderer={rowRenderer} data={data} />);
+    expect(result.trim()).toEqual(expected.trim());
+  });
+});
+
 
 describe('Table Row', () => {
   it('should escape pipe operators in row values', () => {
@@ -8,14 +34,14 @@ describe('Table Row', () => {
       '(root)',
       'string',
       '-',
-      'allowed (`"foo|bar"`)',
-      'pattern (`/foo|bar/`)',
+      'allowed (`"foo|bar|baz"`)',
+      'pattern (`/foo\\|bar/`)',
       '-',
     ];
 
-    const expected = '| (root) | string | - | allowed (`"foo\\|bar"`) | pattern (`/foo\\|bar/`) | - |';
+    const expected = '| (root) | string | - | allowed (`"foo\\|bar\\|baz"`) | pattern (`/foo\\\\|bar/`) | - |';
 
-    const result = render(<TableRow rowRenderer={rowRenderer} entry={null} />);
+    const result = render(<TableRow rowRenderer={rowRenderer} />);
     expect(result.trim()).toEqual(expected.trim());
   });
 });
