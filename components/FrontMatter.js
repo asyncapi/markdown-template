e6271fedb5118e5433d963d55c814a6cfb6df57b
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-11-03 12:03:48 +0100

    update frontmatter

--- components/FrontMatter.js
+++ components/FrontMatter.js
@@ -4,9 +4,10 @@ import * as fs from 'fs';
 import * as yaml from 'yaml';
 
 export function FrontMatter({ asyncapi, params }) {
+  const info = asyncapi.info();
   const frontMatter = yaml.parse(fs.readFileSync(params.frontMatter, 'utf8'));
   let frontMatterStr = yaml.stringify(frontMatter);
-  frontMatterStr = frontMatterStr.split('{{title}}').join(asyncapi.info().title());
-  frontMatterStr = frontMatterStr.split('{{version}}').join(params.version || asyncapi.info().version());
+  frontMatterStr = frontMatterStr.split('{{title}}').join(info.title());
+  frontMatterStr = frontMatterStr.split('{{version}}').join(params.version || info.version());
   return <Text newLines={2}>{`---\n${frontMatterStr}---`}</Text>;
 }
