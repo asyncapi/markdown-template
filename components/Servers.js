16d94ab9c323b54f9b47bca9e557383eaa195d80
sahillllllllll-bit <sahilsingh59443@gmail.com>
2026-01-21 14:00:38 +0000

    fix: render server host without scheme for AsyncAPI v3

--- components/Servers.js
+++ components/Servers.js
@@ -51,7 +51,7 @@ function ServerInfo({ server }) {
   return (
     <Text>
       <Text>
-        <ListItem>URL: `{server.url()}`</ListItem>
+        <ListItem>Host: `{server.host()}${server.pathname() ? server.pathname() : ''}`</ListItem>
         <ListItem>Protocol: `{server.protocol()}{server.protocolVersion() ? ` ${server.protocolVersion()}` : ''}`</ListItem>
       </Text>
       {server.hasDescription() && (
